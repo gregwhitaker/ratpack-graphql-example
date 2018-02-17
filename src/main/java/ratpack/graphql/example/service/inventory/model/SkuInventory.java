eea0d3eef4dfdea4bbdf0f76a920d2323caed36c
gregwhitaker <gwhitake@gmail.com>
2018-02-16 19:13:17 -0800

    Doing work

---
+++ src/main/java/ratpack/graphql/example/service/inventory/model/SkuInventory.java
@@ -0,0 +1,23 @@
+package ratpack.graphql.example.service.inventory.model;
+
+public class SkuInventory {
+
+    private String skuId;
+    private Integer count;
+
+    public String getSkuId() {
+        return skuId;
+    }
+
+    public void setSkuId(String skuId) {
+        this.skuId = skuId;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+}
